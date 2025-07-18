bfc9f0a4daa1e446e0b1993c8c5bdd3be873e536
xavpav9 <192867446+xavpav9@users.noreply.github.com>
2025-07-18 19:35:02 +0100

    Complete task 09

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,15 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+  if (num1 !== parseInt(num1) || num2 !== parseInt(num2) || num1 < 0 || num2 < 0) return "ERROR";
+  let small = (num1 > num2) ? num2 : num1;
+  let large = (num1 > num2) ? num1 : num2;
 
+  let sum = 0;
+  
+  for (; small <= large; ++small) {
+    sum += small;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
