4fdaa637ca871aaffbe4125df39262d7b675dc9e
xavpav9 <192867446+xavpav9@users.noreply.github.com>
2025-07-18 19:44:11 +0100

    Complete task 10

--- 10_leapYears/leapYears.js
+++ 10_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+  if (Number.isInteger(year / 4)) {
+    if (Number.isInteger(year / 100)) {
+      if (Number.isInteger(year / 400)) return true;
+      else return false;
+    }
+    return true;
+  };
+  return false;
 };
 
 // Do not edit below this line
