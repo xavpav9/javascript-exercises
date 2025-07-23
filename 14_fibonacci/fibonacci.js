a91c52b16c04526da8640895d80d43cf317746b5
xavpav9 <192867446+xavpav9@users.noreply.github.com>
2025-07-23 11:28:21 +0100

    Complete task 14
    Use recursive call instead of a loop

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,8 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  if (num < 0) return "OOPS";
+  if (num == 1) return 1;
+  if (num == 0) return 0;
+  return fibonacci(num - 1) + fibonacci(num - 2);
 };
 
 // Do not edit below this line
