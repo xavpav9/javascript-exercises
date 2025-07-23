c85972290fb78ad6647cb0b444039c564a73b5a5
xavpav9 <192867446+xavpav9@users.noreply.github.com>
2025-07-23 11:15:18 +0100

    Complete task 13
    Alphanumerical check is different.

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+  let minimalStr = str.toLowerCase().split("").filter(char => (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) || (char.charCodeAt(0) > 48 && char.charCodeAt(0) <= 57)).join("");
+  return minimalStr.toLowerCase() === minimalStr.split("").reverse().join("").toLowerCase();
 };
 
 // Do not edit below this line
