df958c15a9e0a2be905093d334f72978b2b15acc
xavpav9 <192867446+xavpav9@users.noreply.github.com>
2025-07-23 10:50:47 +0100

    Complete task 12

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,27 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((sum, current) => sum += +current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((sum, current) => sum *= +current, 1);
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	return a**b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+  let product = 1;
+	for (let i = 2; i <= a; ++i) product *= i;
+  return product;
 };
 
 // Do not edit below this line
