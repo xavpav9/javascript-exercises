16048e3f2949067ace91dc0d4ccde7ce80534e3e
xavpav9 <192867446+xavpav9@users.noreply.github.com>
2025-07-23 11:51:06 +0100

    Complete task 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+  return people.slice(1).reduce((oldest, person) => {
+    const newAge = (("yearOfDeath" in person) ? person.yearOfDeath : new Date().getFullYear()) - person.yearOfBirth;
+    return (newAge > oldest[1]) ? [person, newAge] : oldest;
+  }, [people[0], (("yearOfDeath" in people[0]) ? people[0].yearOfDeath : new Date().getFullYear()) - people[0].yearOfBirth] )[0];
 };
 
 // Do not edit below this line
